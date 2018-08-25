21227e305f57377f661633f6161a12da0c362e01
mudkip908 <shadow21222@gmail.com>
2018-08-26 00:06:05 +0200

    Include filename and line number in output of debug build

--- log.h
+++ log.h
@@ -6,13 +6,18 @@ extern enum verbosity_level
 	V_DBG
 } verbosity;
 
-#define LOG(verb, fmt, ...) do{if(verbosity>=verb){printf(fmt "\n", ##__VA_ARGS__);}}while(0)
+
+#define STRINGIFY(x) STRINGIFY2(x)
+#define STRINGIFY2(x) #x
+#ifdef DEBUG
+#define DBG(fmt, ...) LOG(V_DBG, "[DEBG] " fmt, ##__VA_ARGS__)
+#define LOG_PREFIX "[" __FILE__ ":" STRINGIFY(__LINE__) "] "
+#else
+#define DBG(fmt, ...)
+#define LOG_PREFIX ""
+#endif
+
+#define LOG(verb, fmt, ...) do{if(verbosity>=verb){printf(LOG_PREFIX fmt "\n", ##__VA_ARGS__);}}while(0)
 #define INFO(fmt, ...) LOG(V_INFO, "[INFO] " fmt, ##__VA_ARGS__)
 #define WARN(fmt, ...) LOG(V_WARN, "[WARN] " fmt, ##__VA_ARGS__)
 #define ERR(fmt, ...) LOG(V_ERR, "[ERR ] " fmt, ##__VA_ARGS__)
-
-#ifdef DEBUG
-#define DBG(fmt, ...) LOG(V_DBG, "[DEBG] " fmt, ##__VA_ARGS__)
-#else
-#define DBG(fmt, ...)
-#endif
