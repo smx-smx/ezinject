52850046aab9584a6a91b2cf93eca0034f0548cb
mudkip908 <shadow21222@gmail.com>
2018-08-26 22:21:37 +0200

    Add memcpy_{to,from} functions (like {Write,Read}ProcessMemory)

--- util.c
+++ util.c
@@ -1,4 +1,6 @@
+#define _GNU_SOURCE
 #include "util.h"
+#include <sys/uio.h>
 
 void *get_base(pid_t pid, char *libname)
 {
@@ -20,4 +22,20 @@ void *get_base(pid_t pid, char *libname)
 	} while(val > 0 && !found);
 	fclose(fp);
 	return base;
+}
+
+ssize_t memcpy_to(pid_t pid, void *remote_dest, void* local_src, size_t n)
+{
+	struct iovec local_iov = {.iov_base = local_src, .iov_len = n};
+	struct iovec remote_iov = {.iov_base = remote_dest, .iov_len = n};
+
+	return process_vm_writev(pid, &local_iov, 1, &remote_iov, 1, 0);
+}
+
+ssize_t memcpy_from(pid_t pid, void *local_dest, void* remote_src, size_t n)
+{
+	struct iovec local_iov = {.iov_base = local_dest, .iov_len = n};
+	struct iovec remote_iov = {.iov_base = remote_src, .iov_len = n};
+
+	return process_vm_readv(pid, &local_iov, 1, &remote_iov, 1, 0);
 }
--- util.h
+++ util.h
@@ -6,3 +6,5 @@
 #include "log.h"
 
 void *get_base(pid_t pid, char *libname);
+ssize_t memcpy_to(pid_t pid, void *remote_dest, void* local_src, size_t n);
+ssize_t memcpy_from(pid_t pid, void *local_dest, void* remote_src, size_t n);
