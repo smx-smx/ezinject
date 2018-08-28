1613569982e3159b6a4bd754dbacfbd1b379e355
Stefano Moioli <smxdev4@gmail.com>
2018-08-28 13:18:19 +0200

    util: added hexdump

--- util.h
+++ util.h
@@ -6,6 +6,7 @@
 #include "config.h"
 #include "log.h"
 
+void hexdump(void *pAddressIn, long lSize);
 void *get_base(pid_t pid, char *libname);
 ssize_t memcpy_to(pid_t pid, void *remote_dest, void* local_src, size_t n);
 ssize_t memcpy_from(pid_t pid, void *local_dest, void* remote_src, size_t n);
