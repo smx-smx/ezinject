6bb63d4fe5ab59df242b5554d8c05443096bc7e8
Stefano Moioli <smxdev4@gmail.com>
2020-01-04 16:15:02 +0100

    Removed extraneous file

--- arch_ops.h
+++
@@ -1,9 +0,0 @@
-#ifndef __EZ_ARCH_H
-#define __EZ_ARCH_H
-
-extern unsigned char *arch_syscall_ret;
-extern unsigned char *arch_pop_stack;
-extern unsigned char *arch_pop_stack_end;
-extern unsigned char *arch_ret;
-
-#endif
