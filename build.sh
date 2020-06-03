23a65cfb886a1148eb54810b134fe33fe4282ede
Stefano Moioli <smxdev4@gmail.com>
2020-06-03 19:06:50 +0200

    build.sh: fixed toolchain file check

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [ "$1" == "clean" ]; then
 	echo "Removing build directory..."
 	rm -r build
 	exit 0
-elif [ ! -z "$1" ] && [ -f "$1.cmake" ]; then
+elif [ ! -z "$1" ] && [ -f "$1" ]; then
 	TOOLCHAINFILE="-DCMAKE_TOOLCHAIN_FILE=$1"
 fi
 [ ! -d build ] && mkdir build
