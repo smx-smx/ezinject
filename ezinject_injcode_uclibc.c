9e77ceb2398bbd42858893fb53f497b736dbdcc8
Stefano Moioli <smxdev4@gmail.com>
2021-11-23 00:02:50 +0100

    uclibc: implemented symbol scope detection heuristic

--- ezinject_injcode_uclibc.c
+++ ezinject_injcode_uclibc.c
@@ -8,6 +8,40 @@
  */
 #include "ezinject_injcode.h"
 
+#ifndef UCLIBC_OLD
+#define MAX_SCAN_LIMIT 25
+
+#define SIGN_MASK(x) ((intptr_t)(x)) >> ((sizeof(intptr_t) * 8) - 1)
+#define ABS(x) (((intptr_t)(x) + SIGN_MASK(x)) ^ SIGN_MASK(x))
+
+INLINE unsigned _get_global_scope_offset(struct injcode_bearing *br){
+	struct elf_resolve_hdr *elf_tpnt = *(br->uclibc_loaded_modules);
+
+#define IS_PTR(x) ( ((x) != 0) && ABS(PTRDIFF(x, elf_tpnt)) < 0x1000 )
+// if we are still in pointer range after subtracting, not a pointer (small enough value)
+#define IS_NOT_PTR(x) IS_PTR(PTRDIFF(elf_tpnt, x))
+
+	inj_dbgptr(br, elf_tpnt);
+	uintptr_t *pdw = (uintptr_t *)elf_tpnt;
+	for(int i=0; i<MAX_SCAN_LIMIT - 3; i++){
+		if(1
+		&& pdw[i+0] == 1 //libtype == elf_executable
+		&& IS_PTR(pdw[i+1]) //symbol_scope.r_list
+		&& IS_NOT_PTR(pdw[i+2]) && pdw[i+2] > 0 //symbol_scope.n_list
+		&& *(uintptr_t *)(pdw[i+1]) == UPTR(elf_tpnt) //symbol_scope.r_list[0] == elf_tpnt
+		){
+			off_t offset = (i + 1) * sizeof(uintptr_t);
+			return offset;
+		}
+	}
+
+	return 0;
+
+#undef IS_PTR
+#undef IS_NOT_PTR
+}
+#endif
+
 INLINE void *inj_get_libdl(struct injcode_ctx *ctx){
 	struct injcode_bearing *br = ctx->br;
 
@@ -15,11 +49,15 @@ INLINE void *inj_get_libdl(struct injcode_ctx *ctx){
 
 	struct elf_resolve_hdr *tpnt;
 
+	/** adapted from ldso/libdl/libdl.c, do_dlopen **/
+
+	// get to the last symbol chain
 	struct dyn_elf *rpnt;
 	for (rpnt = *(br->uclibc_sym_tables); rpnt && rpnt->next; rpnt = rpnt->next){
 		continue;
 	}
 
+	// calls _dl_load_shared_library, will insert tpnt into rpnt->next
 	tpnt = br->libc_dlopen(0, &rpnt, NULL, libdl_name, 0);
 	if(tpnt == NULL){
 		inj_dchar(br, '!');
@@ -31,12 +69,22 @@ INLINE void *inj_get_libdl(struct injcode_ctx *ctx){
 #endif
 
 #ifndef UCLIBC_OLD
-#define GDB_SHARED_SIZE (5 * sizeof(void *))
-#define SYMBOL_SCOPE_OFFSET (10 * sizeof(void *))
-	struct r_scope_elem *global_scope = (struct r_scope_elem *)(
-		(uintptr_t)*(br->uclibc_loaded_modules) + GDB_SHARED_SIZE +
-		SYMBOL_SCOPE_OFFSET
-	);
+	/**
+	 * we would normally need to build a local symbol scope
+	 * however, since we're in libdl scope, we will steal the global one
+	 **/
+
+	// use an heuristic to locate the symbol scope offset in tpnt
+	unsigned scope_offset = _get_global_scope_offset(br);
+	if(scope_offset == 0){
+		inj_dchar(br, '!');
+		return NULL;
+	}
+
+	struct elf_resolve_hdr *elf_tpnt = *(br->uclibc_loaded_modules);
+	struct r_scope_elem *global_scope = (struct r_scope_elem *)(PTRADD(elf_tpnt, scope_offset));
+	inj_dbgptr(br, global_scope);
+
 #endif
 
 	struct dyn_elf dyn;
