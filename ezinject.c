smx-smx/ezinject

commit 844b0f7faf1e6629ebfce384b4cb0adb8236d45a

macOS: support sandboxed processes
macOS Sonoma introduced a restriction on thread_set_state, which makes it unsuitable for ezinject
when targeting hardened binaries such as system binaries.
Since we can still read/write process memory, and more importantly create remote threads,
we can spawn a new thread insted of hijacking an existing one.
Due to the remote thread being a low level Mach thread (with no TLS), we need some workarounds
(adapted from Yabai) to spawn a Posix thread and cleanup the previous Mach thread.

NOTE: sandboxed processes are restricted from the paths they can read/write to.
The user Trash directory works - and Frida also uses it.

netapi was additionally modified to work behind a sandbox, where bind() might not be possible.
FIFOs are instead used in this case.

netapi's PEEK method was also fixed, since it was broken

ezinject.c
@@ -56,7 +56,7 @@ static ez_region region_pl_code = {
 	.end = (void *)&__stop_payload
 };
 
-#ifdef HAVE_SC
+#ifdef HAVE_SYSCALLS
 uintptr_t get_wrapper_address(struct ezinj_ctx *ctx);
 #endif
 
@@ -132,7 +132,7 @@ intptr_t setregs_syscall(
 		}
 	}
 
-	#ifdef HAVE_SC
+	#ifdef HAVE_SYSCALLS
 	/**
 	 * this target supports true system calls
 	 * use a wrapper in-between to avoid stack corruption
@@ -239,7 +239,12 @@ intptr_t remote_call_common(struct ezinj_ctx *ctx, struct call_req *call){
 		return -1;
 	}
 
-	if(remote_continue(ctx, 0) < 0){
+	if(ctx->pl_debug) {
+		if(remote_detach(ctx) != 0){
+			PERROR("ptrace");
+			return -1;
+		}
+	} else if(remote_continue(ctx, 0) != 0){
 		PERROR("ptrace");
 		return -1;
 	}
@@ -282,7 +287,7 @@ intptr_t remote_call_common(struct ezinj_ctx *ctx, struct call_req *call){
 	#endif
 	} while(wait);
 
-	if(call->syscall_mode == 0 && ctx->pl_debug){
+	if(call->syscall_mode == 0 || ctx->pl_debug){
 		return -1;
 	}
 
@@ -326,6 +331,7 @@ intptr_t remote_call_common(struct ezinj_ctx *ctx, struct call_req *call){
 		}
 	}
 
+#ifndef HAVE_REMOTING
 	if(remote_setregs(ctx, &orig_ctx)){
 		PERROR("remote_setregs failed");
 	}
@@ -335,6 +341,7 @@ intptr_t remote_call_common(struct ezinj_ctx *ctx, struct call_req *call){
 	DBG("PC: %p => %p",
 		(void *)call->insn_addr,
 		(void *)((uintptr_t)REG(new_ctx, REG_PC)));
+#endif
 #endif
 
 	return call->rcall.result;
@@ -625,6 +632,18 @@ struct injcode_bearing *prepare_bearing(struct ezinj_ctx *ctx, int argc, char *a
 #endif
 #endif
 
+#ifdef EZ_TARGET_DARWIN
+	br->pthread_create = (void *)ctx->pthread_create.remote;
+	br->pthread_join = (void *)ctx->pthread_join.remote;
+	br->pthread_create_from_mach_thread = (void *)ctx->pthread_create_from_mach_thread.remote;
+	br->pthread_detach = (void *)ctx->pthread_detach.remote;
+	br->pthread_self = (void *)ctx->pthread_self.remote;
+	br->mach_thread_self = (void *)ctx->mach_thread_self.remote;
+	br->thread_terminate = (void *)ctx->thread_terminate.remote;
+	br->mach_port_allocate = (void *)ctx->mach_port_allocate.remote;
+	br->task_self_trap = (void *)ctx->task_self_trap.remote;
+#endif
+
 #ifdef EZ_TARGET_WINDOWS
 	br->WriteFile = (void *)ctx->write_file.remote;
 	br->LdrRegisterDllNotification = (void *)ctx->nt_register_dll_noti.remote;
@@ -787,6 +806,7 @@ int ezinject_main(
 	uintptr_t r_sc_elf = 0;
 	uintptr_t r_sc_vmem = 0;
 
+	#if defined(HAVE_SHELLCODE)
 	// allocate initial shellcode on the ELF header
 	INFO("target: allocating sc");
 	if(remote_sc_alloc(ctx, SC_ALLOC_ELFHDR, &r_sc_elf) != 0){
@@ -803,6 +823,7 @@ int ezinject_main(
 		ERR("remote_sc_check failed");
 		return -1;
 	}
+	#endif
 
 	intptr_t err = -1;
 	do {
@@ -847,6 +868,7 @@ int ezinject_main(
 		}
 		#endif
 
+		#if !defined(HAVE_REMOTING) && defined(HAVE_SHELLCODE)
 		// allocate new shellcode on a new memory map
 		// the current shellcode is used for the allocation
 		// this must be done before switching to payload mode
@@ -862,6 +884,7 @@ int ezinject_main(
 			ERR("remote_sc_free: ELF header restore failed");
 			return -1;
 		}
+		#endif
 
 		// switch to SIGSTOP wait mode
 		ctx->syscall_mode = 0;
@@ -906,6 +929,7 @@ int ezinject_main(
 		INFO("target: freeing payload memory");
 		remote_pl_free(ctx, remote_shm_ptr);
 
+	#if !defined(HAVE_REMOTING) && defined(HAVE_SHELLCODE)
 		// switch back to the ELF header, to free vmem
 		if(remote_sc_alloc(ctx, SC_ALLOC_ELFHDR, &r_sc_elf) != 0){
 			ERR("remote_sc_alloc: failed to overwrite ELF header");
@@ -918,6 +942,7 @@ int ezinject_main(
 			ERR("remote_sc_free: failed to free memory map");
 			return -1;
 		}
+	#endif
 
 		// now free the ELF header once more (no syscalls allowed after this point)
 		if(remote_sc_free(ctx, SC_ALLOC_ELFHDR, r_sc_elf) != 0){

config.h.in
@@ -33,6 +33,18 @@
 #cmakedefine DL_LIBRARY_NAME "@DL_LIBRARY_NAME@"
 #cmakedefine PTHREAD_LIBRARY_NAME "@PTHREAD_LIBRARY_NAME@"
 
+// NOTE: Windows uses emulated syscalls to map `mmap` to `VirtualAlloc` and `munmap` to `VirtualFree`
 #if (defined(EZ_TARGET_POSIX) || defined(EZ_TARGET_WINDOWS)) && !defined(EZ_TARGET_DARWIN)
-#define HAVE_SC
+#define HAVE_SYSCALLS
+#endif
+
+#ifdef HAVE_SYSCALLS
+#define HAVE_SHELLCODE
+#endif
+
+// platforms that can allocate memory and thread in remote processes
+// NOTE: this includes some versions of Windows (officially Windows XP+).
+// since we want to support Windows 9X and older NTOS versions, we explicitly don't list it here
+#if (defined(EZ_TARGET_DARWIN))
+#define HAVE_REMOTING
 #endif

ezinject.h
@@ -81,8 +81,12 @@ struct ezinj_ctx {
 #ifdef EZ_TARGET_DARWIN
 	task_t task;
 	thread_t thread;
+	// $FIXME: this is kind of a hack, but we only do one allocation
+	// the better idea would be to add a `size` parameter to `remote_pl_free`
+	size_t last_alloc_size;
 #endif
-#if defined(EZ_TARGET_LINUX) || defined(EZ_TARGET_FREEBSD) || defined(EZ_TARGET_WINDOWS)
+#if defined(EZ_TARGET_LINUX) || defined(EZ_TARGET_FREEBSD) || defined(EZ_TARGET_WINDOWS) \
+|| defined(EZ_TARGET_DARWIN)
 	// holds the overwritten ELF header
 	uint8_t *saved_sc_data;
 	ssize_t saved_sc_size;
@@ -97,6 +101,17 @@ struct ezinj_ctx {
 	pfnCallHandler rcall_handler_post;
 	ez_addr libc_syscall;
 	ez_addr libc_dlopen;
+#ifdef EZ_TARGET_DARWIN
+	ez_addr pthread_create_from_mach_thread;
+	ez_addr pthread_create;
+	ez_addr pthread_join;
+	ez_addr pthread_detach;
+	ez_addr pthread_self;
+	ez_addr mach_thread_self;
+	ez_addr task_self_trap;
+	ez_addr mach_port_allocate;
+	ez_addr thread_terminate;
+#endif
 #ifdef EZ_TARGET_LINUX
 	ez_addr libc_mmap;
 	ez_addr libc_open;

ezinject_injcode.c
@@ -269,6 +269,8 @@ INLINE intptr_t inj_load_library(struct injcode_ctx *ctx){
 
 	// fetch argv[0], the library absolute path
 	ctx->userlib_name = BR_STRTBL(br)[EZSTR_ARGV0].str;
+
+	PCALL(ctx, inj_puts, ctx->userlib_name);
 	br->userlib = inj_dlopen(ctx, ctx->userlib_name, RTLD_NOW);
 
 	PCALL(ctx, inj_dbgptr, br->userlib);
@@ -296,29 +298,71 @@ INLINE void inj_plapi_init(struct injcode_call *sc, struct injcode_ctx *ctx){
 	#undef PCOPY
 }
 
+#ifdef EZ_TARGET_DARWIN
+INLINE int _inj_is_first_run(){
+	return br->tid == 0;
+}
+#else
+INLINE int _inj_is_first_run(){
+	return 1;
+}
+#endif
+
 intptr_t PLAPI injected_fn(struct injcode_call *sc){
 	struct injcode_bearing *br = (struct injcode_bearing *)(sc->argv[0]);
 	struct injcode_ctx stack_ctx;
 	struct injcode_ctx *ctx = &stack_ctx;
+
 	sc->plapi.inj_memset(NULL, ctx, 0x00, sizeof(*ctx));
 	inj_plapi_init(sc, ctx);
 
 	ctx->br = br;
 	ctx->stbl = BR_STRTBL(br);
 
-	if(br->pl_debug){
-		return 0;
+	// init the string table, if it's the first time we're called
+	if(_inj_is_first_run()){
+		/** convert string offsets to pointers */
+		char *str_base = (char *)PTRADD(ctx->stbl, sizeof(struct ezinj_str) * br->num_strings);
+		for(unsigned i=0; i<br->num_strings; i++){
+			ctx->stbl[i].str = (char *)PTRADD(ctx->stbl[i].str, str_base);
+		}
 	}
 
-	/** convert string offsets to pointers */
-	char *str_base = (char *)PTRADD(ctx->stbl, sizeof(struct ezinj_str) * br->num_strings);
-	for(unsigned i=0; i<br->num_strings; i++){
-		ctx->stbl[i].str = (char *)PTRADD(ctx->stbl[i].str, str_base);
+	#if 0
+	if(br->pl_debug){
+		asm volatile(JMP_INSN " .");
 	}
+	#endif
+
 
 	intptr_t result = 0;
 	// entry
 	PCALL(ctx, inj_dchar, 'e');
+
+	#ifdef EZ_TARGET_DARWIN
+	int thread_is_parent = br->tid == 0;
+	if(thread_is_parent){
+		PCALL(ctx, inj_dchar, 't');
+
+		sc->mach_thread = br->mach_thread_self();
+
+		// sc->trampoline.fn_addr
+		if(br->pthread_create_from_mach_thread(&br->tid, NULL, (void * (*)(void *))injected_fn, sc) != 0){
+			PCALL(ctx, inj_dchar, '!');
+			result = INJ_ERR_DARWIN_THREAD;
+			goto pl_exit;
+		}
+		goto pl_exit;
+	} else {
+		// detach ourselves to free resources
+		// (the parent can't do it because it has no TLS)
+		br->pthread_detach(br->pthread_self());
+
+		// kill the parent thread
+		// (the parent can't do it because it has no TLS within `thread_terminate`)
+		br->thread_terminate(sc->mach_thread);
+	}
+	#endif
 
 	ctx->libdl_name = BR_STRTBL(br)[EZSTR_API_LIBDL].str;
 	ctx->libpthread_name = BR_STRTBL(br)[EZSTR_API_LIBPTHREAD].str;
@@ -333,7 +377,6 @@ intptr_t PLAPI injected_fn(struct injcode_call *sc){
 	PCALL(ctx, inj_dchar, 'p');
 
 	PCALL(ctx, inj_puts, ctx->libpthread_name);
-	//asm volatile(JMP_INSN " .");
 	ctx->h_libthread = inj_dlopen(ctx, ctx->libpthread_name, RTLD_LAZY | RTLD_GLOBAL);
 	if(!ctx->h_libthread){
 		PCALL(ctx, inj_dchar, '!');
@@ -369,6 +412,8 @@ intptr_t PLAPI injected_fn(struct injcode_call *sc){
 		char *errstr = NULL;
 		if(ctx->libdl.dlerror && (errstr=ctx->libdl.dlerror()) != NULL){
 			PCALL(ctx, inj_puts, errstr);
+		} else {
+			PCALL(ctx, inj_dchar, '?');
 		}
 		result = INJ_ERR_DLOPEN;
 		goto pl_exit;
@@ -407,8 +452,22 @@ pl_exit:
 		ctx->libdl.dlclose(ctx->h_libthread);
 	}*/
 
+	#ifdef EZ_TARGET_DARWIN
+	if(thread_is_parent){
+		// it looks we can't kill a thread created from `thread_create_running`, so we do hacks
+		asm volatile(JMP_INSN " .");
+		//return 0;
+	} else {
+		sc->result = result;
+		br->libc_syscall(__NR_kill,
+			br->libc_syscall(__NR_getpid),
+			SIGSTOP);
+		return 0;
+	}
+	#else
 	// return to ezinject
 	PL_RETURN(sc, result);
+	#endif
 
 	asm volatile(JMP_INSN " .");
 

ezinject_injcode.h
@@ -25,6 +25,12 @@
 */
 #endif
 
+#ifdef EZ_TARGET_DARWIN
+#include <mach/mach.h>
+#include <mach/mach_init.h>
+#include <mach/thread_act.h>
+#endif
+
 #define SC_MAX_ARGS 8
 #include "ezinject_common.h"
 
@@ -167,6 +173,9 @@ struct injcode_call {
 	DWORD WINAPI (*SuspendThread)(HANDLE hThread);
 	HANDLE WINAPI (*GetCurrentThread)(VOID);
 #endif
+#ifdef EZ_TARGET_DARWIN
+	thread_act_t mach_thread;
+#endif
 
 	uintptr_t ezstate;
 
@@ -217,6 +226,29 @@ struct injcode_bearing
 	HANDLE hEvent;
 #endif
 	void *userlib;
+
+#if defined(EZ_TARGET_DARWIN)
+	pthread_t tid;
+
+	int (*pthread_create)(pthread_t *restrict thread,
+		const pthread_attr_t *restrict attr,
+		typeof(void *(void *)) *start_routine,
+		void *restrict arg);
+	int (*pthread_join)(pthread_t thread, void **value_ptr);
+	int (*pthread_create_from_mach_thread)(
+		pthread_t *thread,
+		const pthread_attr_t *attr,
+		void *(*start_routine)(void *), void *arg);
+	pthread_t (*pthread_self)(void);
+	int (*pthread_detach)(pthread_t thread);
+	kern_return_t (*thread_terminate)(thread_act_t target_act);
+	kern_return_t (*mach_port_allocate)
+		(ipc_space_t        task,
+		mach_port_right_t   right,
+		mach_port_name_t    *name);
+	thread_act_t (*mach_thread_self)(void);
+	mach_port_t  (*task_self_trap)(void);
+#endif
 
 #if defined(HAVE_DL_LOAD_SHARED_LIBRARY)
 	void *(*libc_dlopen)(unsigned rflags, struct dyn_elf **rpnt,
@@ -402,5 +434,6 @@ extern uint8_t __stop_syscall SECTION_END("syscall");
 #define INJ_ERR_API 3
 #define INJ_ERR_DLOPEN 4
 #define INJ_ERR_WAIT 5
+#define INJ_ERR_DARWIN_THREAD 6
 
 #endif

ezinject_injcode_darwin.c
@@ -7,6 +7,7 @@
  *  3. This notice may not be removed or altered from any source distribution.
  */
 
+#undef PL_RETURN
 #define PL_RETURN(sc, x) do { \
 	((sc)->result = (x)); \
 	sc->libc_syscall(__NR_kill, \

ezinject_injcode_posix.c
@@ -74,16 +74,26 @@ INLINE intptr_t _inj_init_libdl(struct injcode_ctx *ctx){
 
 	// just to make sure it's really loaded
 	ctx->h_libdl = ctx->libdl.dlopen(ctx->libdl_name, RTLD_NOLOAD);
-	PCALL(ctx, inj_dbgptr, ctx->h_libdl);
 	if(ctx->h_libdl == NULL){
 		ctx->h_libdl = ctx->libdl.dlopen(ctx->libdl_name, RTLD_NOW | RTLD_GLOBAL);
 	}
+	PCALL(ctx, inj_dbgptr, ctx->h_libdl);
 
 	if(ctx->h_libdl == NULL){
 		return -1;
 	}
 
+#ifdef EZ_TARGET_DARWIN
+	void *h_self = ctx->libdl.dlopen(NULL, RTLD_LAZY);
+	if(h_self == NULL){
+		return -1;
+	}
+	intptr_t res = PCALL(ctx, inj_fetchsym, EZSTR_API_DLERROR, h_self, (void **)&ctx->libdl.dlerror);
+	ctx->libdl.dlclose(h_self);
+	return res;
+#else
 	return PCALL(ctx, inj_fetchsym, EZSTR_API_DLERROR, ctx->h_libdl, (void **)&ctx->libdl.dlerror);
+#endif
 }
 
 INLINE intptr_t inj_api_init(struct injcode_ctx *ctx){

os/darwin/inject.c
@@ -43,11 +43,27 @@ uintptr_t remote_pl_alloc(struct ezinj_ctx *ctx, size_t mapping_size){
 		ERR("vm_protect failed to set current page privs: %s", mach_error_string(kr));
 		return 0;
 	}
+	ctx->last_alloc_size = mapping_size;
 	return address;
 }
 
 EZAPI remote_pl_free(struct ezinj_ctx *ctx, uintptr_t remote_shmaddr){
 	return 0;
+	/*
+	kern_return_t kr = vm_deallocate(
+		ctx->task,
+		(vm_address_t)remote_shmaddr,
+		ctx->last_alloc_size
+	);
+	if(kr != KERN_SUCCESS){
+		ERR("vm_deallocate failed");
+		return -1;
+	}
+	return 0;
+	*/
 }
 
-EZAPI remote_call_prepare(struct ezinj_ctx *ctx, struct injcode_call *call){ return 0; }
+EZAPI remote_call_prepare(struct ezinj_ctx *ctx, struct injcode_call *call){
+	return 0;
+}
+

commit bb6b6ee9e8b8e596cf4c0bad9630a16ec971f033

fix broken thread restore regression caused by a change in 844b0f7faf1e6629ebfce384b4cb0adb8236d45a

ezinject.c
@@ -287,7 +287,7 @@ intptr_t remote_call_common(struct ezinj_ctx *ctx, struct call_req *call){
 	#endif
 	} while(wait);
 
-	if(call->syscall_mode == 0 || ctx->pl_debug){
+	if(ctx->pl_debug){
 		return -1;
 	}
 