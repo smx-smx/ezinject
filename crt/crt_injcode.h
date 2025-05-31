8f8b360fbffb8ce8f3d9edd2807ca01627e9f4cd
Stefano Moioli <smxdev4@gmail.com>
2025-05-31 11:57:16 +0200

    add WINAPI annotations for Windows 32bit

--- crt/crt_injcode.h
+++ crt/crt_injcode.h
@@ -41,15 +41,15 @@ struct inj_unload_call {
 #elif defined(EZ_TARGET_WINDOWS)
     HANDLE caller_thread;
     HANDLE cond;
-    BOOL (*CloseHandle)(HANDLE hObject);
-    DWORD (*WaitForSingleObject)(
+    BOOL WINAPI (*CloseHandle)(HANDLE hObject);
+    DWORD WINAPI (*WaitForSingleObject)(
         HANDLE hHandle,
         DWORD  dwMilliseconds
     );
     WINBOOL WINAPI (*FreeLibrary)(HMODULE hLibModule);
     VOID WINAPI (*ExitThread)(DWORD dwExitCode);
-    BOOL (*SetEvent)(HANDLE hEvent);
-    BOOL (*VirtualProtect)(
+    BOOL WINAPI (*SetEvent)(HANDLE hEvent);
+    BOOL WINAPI (*VirtualProtect)(
         LPVOID lpAddress,
         SIZE_T dwSize,
         DWORD  flNewProtect,
