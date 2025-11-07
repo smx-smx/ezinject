smx-smx/ezinject

commit 9e197054509e04d941a779f62d2cbd35d152aac6

add os_realpath, refactor push_string

ezinject.c
@@ -510,49 +510,49 @@ int libc_init(struct ezinj_ctx *ctx){
 }
 
 int push_string(
+	struct ezinj_strings *strings,
 	enum ezinj_str_id str_id,
-	struct ezinj_str **pArgs,
-	unsigned *num_strings, unsigned *capacity,
-	size_t *dyn_str_size,
 	const char *str
 ){
-	unsigned index = MAX(str_id, *num_strings);
-	if(index >= *capacity){
+	unsigned index = MAX(str_id, strings->num_strings);
+	if(index >= strings->argsLim){
 		// insert index + 128 new items
-		*capacity = index + 128;
-		*pArgs = realloc(*pArgs, sizeof(struct ezinj_str) * *capacity);
-		if(*pArgs == NULL){
+		strings->argsLim = index + 128;
+		strings->args = realloc(strings->args, sizeof(struct ezinj_str) * strings->argsLim);
+		if(strings->args == NULL){
 			PERROR("realloc");
 			return -1;
 		}
 	}
 	size_t str_size = 0;
-	(*pArgs)[str_id] = ezstr_new(str_id, str, &str_size);
-	*dyn_str_size += str_size;
+	strings->args[str_id] = ezstr_new(str_id, str, &str_size);
+	strings->dyn_str_size += str_size;
 	return 0;
 }
 
 struct injcode_bearing *prepare_bearing(struct ezinj_ctx *ctx, int argc, char *argv[]){
-	size_t dyn_ptr_size = argc * sizeof(char *);
-	size_t dyn_str_size = 0;
+	struct ezinj_strings strings = {
+		.dyn_str_size = 0,
+		.num_strings = EZSTR_MAX_DEFAULT,
+	};
 
-	unsigned argsLim = 128;
-	struct ezinj_str *args = calloc(argsLim, sizeof(struct ezinj_str));
-	if(!args){
-		PERROR("calloc");
-		return NULL;
+	{
+		unsigned argsLim = 128;
+		struct ezinj_str *args = calloc(argsLim, sizeof(struct ezinj_str));
+		if(!args){
+			PERROR("calloc");
+			return NULL;
+		}
+		strings.args = args;
+		strings.argsLim = argsLim;
 	}
 
-	unsigned num_strings = EZSTR_MAX_DEFAULT;
-
-#define PUSH_STRING(id, str) do { \
-	if(push_string(id, &args, &num_strings, &argsLim, &dyn_str_size, str) < 0) { \
-		return NULL; \
-	} \
-} while(0)
+	intptr_t rc = -1;
+#define PUSH_STRING(id, str) \
+	if((rc=push_string(&strings, id, str)) < 0) return NULL;
 
 #ifdef EZ_TARGET_LINUX
-	off_t pl_filename_offset = dyn_str_size;
+	off_t pl_filename_offset = strings.dyn_str_size;
 	/**
 	 * construct tempory payload filename
 	 * yes, we use tempnam as we don't know
@@ -568,18 +568,11 @@ struct injcode_bearing *prepare_bearing(struct ezinj_ctx *ctx, int argc, char *a
 #endif
 
 	// library to load
-	char libName[PATH_MAX];
-#if defined(EZ_TARGET_POSIX)
-	if(!realpath(argv[0], libName)) {
-		ERR("realpath: %s", libName);
-		PERROR("realpath");
+	char *libName = os_realpath(argv[0]);
+	if(!libName){
+		ERR("realpath(%s) failed", argv[0]);
 		return NULL;
 	}
-#elif defined(EZ_TARGET_WINDOWS)
-	{
-		GetFullPathNameA(argv[0], sizeof(libName), libName, NULL);
-	}
-#endif
 
 	// libdl.so name (without path)
 	PUSH_STRING(EZSTR_API_LIBDL, ctx->libdl_name);
@@ -601,30 +594,27 @@ struct injcode_bearing *prepare_bearing(struct ezinj_ctx *ctx, int argc, char *a
 	PUSH_STRING(EZSTR_API_GET_EXIT_CODE_THREAD, "GetExitCodeThread");
 #endif
 
-char logPath[PATH_MAX];
-if(ctx->module_logfile && strlen(ctx->module_logfile) > 0){
-	// make sure the log file exists before we call realpath on it
-	FILE *fh = fopen(ctx->module_logfile, "a");
-	if(!fh){
-		ERR("fopen \"%s\" failed", ctx->module_logfile);
-		return NULL;
-	}
-	fclose(fh);
+	char *logPath = NULL;
 
-#if defined(EZ_TARGET_POSIX)
-	if(!realpath(ctx->module_logfile, logPath)) {
-		ERR("realpath: %s", logPath);
-		PERROR("realpath");
-		return NULL;
+	if(ctx->module_logfile && strlen(ctx->module_logfile) > 0){
+		// make sure the log file exists before we call realpath on it
+		FILE *fh = fopen(ctx->module_logfile, "a");
+		if(!fh){
+			ERR("fopen \"%s\" failed", ctx->module_logfile);
+			return NULL;
+		}
+		fclose(fh);
+
+		char *logPath = os_realpath(ctx->module_logfile);
+		if(!logPath){
+			ERR("realpath(%s) failed", ctx->module_logfile);
+			return NULL;
+		}
+	
+		PUSH_STRING(EZSTR_LOG_FILEPATH, logPath);
+	} else {
+		PUSH_STRING(EZSTR_LOG_FILEPATH, "");
 	}
-#elif defined(EZ_TARGET_WINDOWS)
-	{
-		GetFullPathNameA(ctx->module_logfile, sizeof(logPath), logPath, NULL);
-	}
-#endif
-} else {
-	strncpy(logPath, "", sizeof(logPath));
-}
 
 	PUSH_STRING(EZSTR_API_CRT_INIT, "crt_init");
 	PUSH_STRING(EZSTR_LOG_FILEPATH, logPath);
@@ -635,21 +625,22 @@ if(ctx->module_logfile && strlen(ctx->module_logfile) > 0){
 	// user arguments
 	for(int i=1; i < argc; i++){
 		PUSH_STRING(EZSTR_ARGV0+i, argv[i]);
-		++num_strings;
+		++strings.num_strings;
 	}
 
 
 #undef PUSH_STRING
 
-	size_t dyn_entries_size = num_strings * sizeof(struct ezinj_str);
-	size_t dyn_total_size = dyn_ptr_size + dyn_entries_size + dyn_str_size;
+	size_t dyn_ptr_size = argc * sizeof(char *);
+	size_t dyn_entries_size = strings.num_strings * sizeof(struct ezinj_str);
+	size_t dyn_total_size = dyn_ptr_size + dyn_entries_size + strings.dyn_str_size;
 	size_t mapping_size = create_layout(ctx, dyn_total_size, &ctx->pl);
 
 	struct injcode_bearing *br = (struct injcode_bearing *)ctx->mapped_mem.local;
 	memset(br, 0x00, sizeof(*br));
 
 	if(!br){
-		free(args);
+		free(strings.args);
 		PERROR("malloc");
 		return NULL;
 	}
@@ -706,7 +697,7 @@ if(ctx->module_logfile && strlen(ctx->module_logfile) > 0){
 
 	br->argc = argc;
 	br->dyn_total_size = dyn_total_size;
-	br->num_strings = num_strings;
+	br->num_strings = strings.num_strings;
 #ifdef EZ_TARGET_LINUX
 	br->pl_filename_offset = pl_filename_offset;
 #endif
@@ -716,17 +707,17 @@ if(ctx->module_logfile && strlen(ctx->module_logfile) > 0){
 
 	struct ezinj_str *pEntries = (struct ezinj_str *)stringEntries;
 	size_t strtbl_off = 0;
-	for(unsigned i=0; i<num_strings; i++){
-		pEntries->id = args[i].id;
+	for(unsigned i=0; i<strings.num_strings; i++){
+		pEntries->id = strings.args[i].id;
 		// store the current string offset. it will rebased by injcode
 		pEntries->str = (char *)strtbl_off;
 		++pEntries;
 
-		if(!args[i].str){
+		if(!strings.args[i].str){
 			continue;
 		}
-		size_t str_sz = STRSZ(args[i].str);
-		memcpy(stringData, args[i].str, str_sz);
+		size_t str_sz = STRSZ(strings.args[i].str);
+		memcpy(stringData, strings.args[i].str, str_sz);
 		stringData += str_sz;
 		strtbl_off += str_sz;
 	}
@@ -735,9 +726,16 @@ if(ctx->module_logfile && strlen(ctx->module_logfile) > 0){
 	free(pl_filename);
 #endif
 
+	if(libName){
+		free(libName);
+	}
+	if(logPath){
+		free(logPath);
+	}
+
 	// copy code
 	memcpy(ctx->pl.code_start, region_pl_code.start, REGION_LENGTH(region_pl_code));
-	free(args);
+	free(strings.args);
 	return br;
 }
 

ezinject.h
@@ -56,6 +56,13 @@ struct ezinj_pl {
 	uint8_t *stack_top;
 };
 
+struct ezinj_strings {
+	struct ezinj_str *args;
+	unsigned num_strings;
+	unsigned argsLim;
+	size_t dyn_str_size;
+};
+
 struct ezinj_ctx;
 
 #define PL_REMOTE(ctx, addr) (ctx->mapped_mem.remote + PTRDIFF(addr, ctx->mapped_mem.local))

os/posix/CMakeLists.txt
@@ -4,4 +4,8 @@ if(EZ_TARGET_POSIX)
 endif()
 
 add_library(os_posix_shellcode inject.c)
-target_link_libraries(${EZINJECT_TARGET} os_posix_shellcode)
+add_library(os_posix_util util.c)
+
+target_link_libraries(${EZINJECT_TARGET}
+	os_posix_shellcode
+	os_posix_util)

os/posix/util.c
@@ -1,17 +1,14 @@
 /*
- * Copyright (C) 2021 Stefano Moioli <smxdev4@gmail.com>
+ * Copyright (C) 2025 Stefano Moioli <smxdev4@gmail.com>
  * This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
  * Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
  *  1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
  *  2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
  *  3. This notice may not be removed or altered from any source distribution.
  */
-#include <stdio.h>
-#include <stdint.h>
-#include <sys/types.h>
 
-#include "ezinject.h"
+#include <stdlib.h>
 
-void hexdump(void *pAddressIn, long lSize);
-ez_addr sym_addr(void *handle, const char *sym_name, ez_addr lib);
-char *os_realpath(const char *path);
+char *os_realpath(const char *path){
+	return realpath(path, NULL);
+}

os/windows/CMakeLists.txt
@@ -2,11 +2,15 @@ add_library(os_windows
 	inject.c
 	libc_windows.c
 	remote.c
-	util.c util.h
 )
+add_library(os_windows_util util.c util.h)
 
 add_subdirectory(InjLib)
-target_link_libraries(${EZINJECT_TARGET} os_windows windows_injlib)
+target_link_libraries(${EZINJECT_TARGET}
+	os_windows
+	windows_injlib
+	os_windows_util
+)
 
 install(
 	FILES util.h

os/windows/util.c
@@ -316,3 +316,19 @@ void *get_base(struct ezinj_ctx *ctx, pid_t pid, const char *substr, const char 
 		return get_base_toolhelp(pid, substr, ignores);
 	}
 }
+
+char *os_realpath(const char *path){
+	DWORD nChars = GetFullPathNameA(path, 0, NULL, NULL);
+	if(nChars == 0){
+		PERROR("GetFullPathNameA");
+		return NULL;
+	}
+	char *buffer = malloc(nChars + 1);
+	buffer[nChars] = '\0';
+	if(GetFullPathNameA(path, nChars, buffer, NULL) == 0){
+		PERROR("GetFullPathNameA");
+		free(buffer);
+		return NULL;
+	}
+	return buffer;
+}

commit c8a3b215325f5d578ba152d3fe66d6fbbe1672aa

fix regression: log file path not working regression introduced by 9e197054509e04d941a779f62d2cbd35d152aac6 which caused the string to be always overwritten by an empty string

ezinject.c
@@ -704,7 +704,7 @@ struct injcode_bearing *prepare_bearing(struct ezinj_ctx *ctx, int argc, char *a
 		}
 		fclose(fh);
 
-		char *logPath = os_realpath(ctx->module_logfile);
+		logPath = os_realpath(ctx->module_logfile);
 		if(!logPath){
 			ERR("realpath(%s) failed", ctx->module_logfile);
 			return NULL;
@@ -716,7 +716,6 @@ struct injcode_bearing *prepare_bearing(struct ezinj_ctx *ctx, int argc, char *a
 	}
 
 	PUSH_STRING(EZSTR_API_CRT_INIT, "crt_init");
-	PUSH_STRING(EZSTR_LOG_FILEPATH, logPath);
 
 	// argv0
 	PUSH_STRING(EZSTR_ARGV0, libName);